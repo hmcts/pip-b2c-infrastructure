221532651f113ba0dcd629994a70daf0ee78dbae
Christopher Pateman <christopher.pateman@version1.com>
2022-02-02 15:51:12 +0000

    Add b2c

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 locals {
-  prefix               = "${var.product}-ss"
+  prefix               = "${var.product}-ss-b2c"
   prefix_no_special    = replace(local.prefix, "-", "")
   env_long_name        = var.env == "sbox" ? "sandbox" : var.env == "stg" ? "staging" : var.env
-  support_env          = var.env == "prod" || var.env == "demo" || var.env == "sbox" ? var.env : "stg"
 }
 data "azurerm_client_config" "current" {}
 
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.prefix}-rg"
+  location = var.location
+  tags     = var.common_tags
+}
---
+++ tf-b2c-main.tf
@@ -0,0 +1,12 @@
+locals {
+  b2c_name_suffix = var.env == "prod" ? "" : "Non-Prod ${var.env}"
+}
+
+resource "azurerm_aadb2c_directory" "directory" {
+  country_code            = "GB"
+  data_residency_location = "Europe"
+  display_name            = "Publication and Information Directory ${local.b2c_name_suffix}"
+  domain_name             = "${local.prefix}-${var.env}.onmicrosoft.com"
+  resource_group_name     = azurerm_resource_group.rg.name
+  sku_name                = "PremiumP1"
+}
