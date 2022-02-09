907d381a2f3c1d5a5d8f0b6dd7a150c433d77185
Christopher Pateman <christopher.pateman@version1.com>
2022-02-09 13:17:41 +0000

    update display name

--- tf-b2c-main.tf
+++ tf-b2c-main.tf
@@ -6,7 +6,7 @@ locals {
 resource "azurerm_aadb2c_directory" "directory" {
   country_code            = "GB"
   data_residency_location = "Europe"
-  display_name            = "Publication and Information Directory ${local.b2c_name_suffix}"
+  display_name            = "HM Courts and Tribunals Hearing Service ${local.b2c_name_suffix}"
   domain_name             = local.b2c_domain_name
   resource_group_name     = azurerm_resource_group.rg.name
   sku_name                = var.b2c_sku
